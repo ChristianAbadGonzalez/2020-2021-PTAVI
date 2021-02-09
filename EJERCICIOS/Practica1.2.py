ee1cc087be6898c1da1efde9e0eec80c48b450bf
Christian Abad González <c.abad.2017@alumnos.urjc.es>
2021-02-09 18:28:39 +0100

    Fallo de subidas de las carpetas

--- EJERCICIOS/Practica1.2.py
+++
@@ -1,8 +0,0 @@
-fich = open('/etc/passwd')
-lineas = fich.readlines()
-fich.close()
-nlineas = len(lineas)
-
-for linea in lineas:
-    palabras = linea.split(':')
-    print(palabras[0] + ': ' + palabras[-1])
